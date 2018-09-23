886104c6bfb577188ac021b33a35afaa43c6ffd3
NickP <chepik29rus@gmail.com>
2018-09-23 18:34:23 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'general/info', to:'general#info'
-  get 'general/info', to: 'general#info'
+  
 
   resources :contest_nomination do 
   	resources:contest_work
