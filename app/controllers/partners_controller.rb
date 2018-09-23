12736b596a01d0a553f7689ef6657ef1b8283fca
Sergey Luschik <frostbolt@live.ru>
2018-09-23 14:09:41 +0300

    partners controller and exception handler

---
+++ app/controllers/partners_controller.rb
@@ -0,0 +1,38 @@
+class PartnersController < ApplicationController
+	before_action :set_partner, only: [:show, :update, :destroy]
+
+	def index
+		@partners = Partner.all
+		json_response @partners
+	end
+
+	def create
+		@partner = Partner.create! partner_params
+		json_response @partner, :created
+	end
+
+	def show
+		json_response @partner
+	end
+
+	def update
+		@partner.update partner_params
+		head :no_content		
+	end
+
+	def destroy
+		@partner.destroy
+		head :no_content		
+	end
+
+	private
+
+	def partner_params
+		params.permit :title, :created_by
+	end
+
+	def set_partner
+		@partner = Partner.find params[:id]
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'general/info', to:'general#info'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'general/info', to:'general#info'
+
+  resource :partners
 end
