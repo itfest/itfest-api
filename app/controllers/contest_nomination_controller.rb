cb8203d0d948320a6972df9abab4d28bd9f1ac79
NickP <chepik29rus@gmail.com>
2018-09-23 20:01:25 +0300

    contestWork controller + small fixes+ rspec

--- app/controllers/contest_nomination_controller.rb
+++ app/controllers/contest_nomination_controller.rb
@@ -1,37 +1,37 @@
 class ContestNominationController < ApplicationController
-  before_action :set_contest_nomination, only: [:show, :edit, :update, :destroy]
+	before_action :set_contest_nomination, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @contest_nomination = ContestNomination.all
-    json_response @contest_nomination
-  end
+	def index
+		@contest_nomination = ContestNomination.all
+		json_response @contest_nomination
+	end
 
-  def create
-      @contest_nomination= ContestNomination.create! contest_nom_params
-      json_response @contest_nomination, :created
-    end
+	def create
+		@contest_nomination= ContestNomination.create! contest_nom_params
+		json_response @contest_nomination, :created
+	end
 
-  def show
-    json_response @contest_nomination
-  end
+	def show
+		json_response @contest_nomination
+	end
 
-  def update
-    @contest_nomination.update contest_nom_params
-    head :no_content    
-  end
+	def update
+		@contest_nomination.update contest_nom_params
+		head :no_content    
+	end
 
-  def destroy
-    @contest_nomination.destroy
-    head :no_content    
-  end
+	def destroy
+		@contest_nomination.destroy
+		head :no_content    
+	end
 
-  private
+	private
 
-  def  contest_nom_params
-    params.permit :name
-  end
+	def contest_nom_params
+		params.permit :name
+	end
 
-  def set_contest_nomination
-    @contest_nomination = ContestNomination.find params[:id]
-  end
+	def set_contest_nomination
+		@contest_nomination = ContestNomination.find params[:id]
+	end
 end
--- app/controllers/contest_work_controller.rb
+++ app/controllers/contest_work_controller.rb
@@ -1,16 +1,37 @@
 class ContestWorkController < ApplicationController
-  def index
-  end
+	before_action :set_contest_work, only: [:show, :edit, :update, :destroy]
 
-  def create
-  end
+	def index
+		@contest_work = ContestWork.all
+		json_response @contest_work
+	end
 
-  def show
-  end
+	def create
+		@contest_work= ContestWork.create! contest_work_params
+		json_response @contest_work, :created
+	end
 
-  def update
-  end
+	def show
+		json_response @contest_work
+	end
 
-  def destroy
-  end
+	def update
+		@contest_work.update contest_work_params
+		head :no_content  
+	end
+
+	def destroy
+		@contest_work.destroy
+		head :no_content  
+	end
+
+	private
+
+	def contest_work_params
+		params.permit :first_namme, :last_name, :patronymic, :university, :speciality, :year_of_study, :group, :email, :phone
+	end
+
+	def set_contest_work
+		@contest_work= ContestWork.find params[:id]
+	end
 end
