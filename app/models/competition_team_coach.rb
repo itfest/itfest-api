f1ff84776ce523c316ad0a3358125f96bddd711c
NickP <chepik29rus@gmail.com>
2018-10-02 18:31:24 +0300

    model coach+serializer

--- app/models/competition_team_coach.rb
+++ app/models/competition_team_coach.rb
@@ -1,2 +1,15 @@
 class CompetitionTeamCoach < ApplicationRecord
+	belongs_to :competition_team
+	validates :first_name, presence: true, length: {in: 2..16}, custom_name: true
+	validates :last_name, presence: true, length: {in: 2..16}, custom_name: true 
+	validates :patronymic, presence: true, length: {in: 2..16}, custom_name: true
+	validates :birthdate, presence: true
+	validates :workplace, presence: true
+	validates :position, presence: true
+	validates :address, presence: true
+	validates :soc_media, presence: true
+	validates :email, presence: true, uniqueness: true, email: true
+	validates :phone, presence: true
+	validates :competition_team, presence: true
+	validates_associated :competition_team
 end
--- app/serializers/competition_team_serializer.rb
+++ app/serializers/competition_team_serializer.rb
@@ -1,4 +1,5 @@
 class CompetitionTeamSerializer < ActiveModel::Serializer
   attributes :id, :name, :notes, :how_learned_about
   has_many :competition_team_members
+  has_one :competition_team_coach
 end
