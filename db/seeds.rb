f8e97fe89f5fbb2f6a8f6d45ff337d33ebf82902
Sergey Luschik <frostbolt@live.ru>
2018-10-03 00:55:40 +0300

    add seeds configuration for CompetitionTeams & ContestWorks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+5.times do 
+	team = CompetitionTeam.create ({
+		name: Faker::Name.middle_name,
+		how_learned_about: Faker::Lorem.word,
+		notes: Faker::Lorem.word
+	})
+
+	3.times do
+		CompetitionTeamMember.create({
+			first_name: Faker::Name.first_name,
+			last_name: Faker::Name.last_name,
+			patronymic: Faker::Name.middle_name,
+			birthdate: Faker::Date.backward(13),
+			university: Faker::Name.middle_name,
+			speciality: Faker::Lorem.word,
+			address: Faker::Lorem.word,
+			year_of_study: Faker::Number.between(1,6),
+			group: Faker::Number.number(6),
+			email: Faker::Internet.email,
+			phone: Faker::PhoneNumber.cell_phone,
+			competition_team: team
+		})
+	end
+
+	nom = ContestNomination.create({
+		caption: Faker::Company.name,
+		availible_for_registration: Faker::Boolean.boolean
+	})
+
+	3.times do
+		ContestWork.create({
+			first_name: Faker::Name.first_name,
+			last_name: Faker::Name.last_name,
+			patronymic: Faker::Name.middle_name,
+			university: Faker::Name.middle_name,
+			year_of_study: Faker::Number.between(1,6),
+			group: Faker::Number.number(6),
+			email: Faker::Internet.email,
+			phone: Faker::PhoneNumber.cell_phone,
+			contest_nomination: nom
+		})
+	end
+end
