8b74888203bf5abf7bfab269c2f1624c44585f5c
NickP <chepik29rus@gmail.com>
2018-10-09 00:03:27 +0300

    hide routes for non-admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,37 @@ Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'general/info', to: 'general#info'
-  put 'general/info', to: 'general#info'
-  patch 'general/info', to: 'general#info'
+  # get 'general/info', to: 'general#info'
+  # put 'general/info', to: 'general#info'
+  # patch 'general/info', to: 'general#info'
   
-  resources :partners
-  resources :annotations
-  resources :information_elements
+  authenticate :admin do
+    scope "/admin" do
+      resources :partners, only: [:new, :create, :edit, :update, :destroy]
+      resources :annotations, only: [:new, :create, :edit, :update, :destroy]
+      resources :information_elements, only: [:new, :create, :edit, :update, :destroy]
+      resources :general_infos, only: [:create,:update]
 
-  resources :contest_nominations do 
-    resources :contest_works
-  end
+      resources :contest_nominations do 
+        resources :contest_works
+      end
 
-  resources :competition_teams do 
-    resources :competition_team_members
+      resources :competition_teams do 
+        resources :competition_team_members
+      end
+    end
   end
+  resources :partners, only: [:index, :show]
+  resources :annotations, only: [:index, :show]
+  resources :information_elements, only: [:index, :show]
+ 
+
+  # resources :contest_nominations do 
+  #   resources :contest_works
+  # end
+
+  # resources :competition_teams do 
+  #   resources :competition_team_members
+  # end
 
 end
