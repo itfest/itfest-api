6fe40a29d9f974ce05b9d42dcf0d17e2a0c565da
NickP <chepik29rus@gmail.com>
2018-10-13 16:51:51 +0300

    small fixes of routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,14 @@ Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get 'general/info', to: 'general#info'
-  # put 'general/info', to: 'general#info'
-  # patch 'general/info', to: 'general#info'
+  get 'general/info', to: 'general#info'
+
   authenticate :admin do
     scope "/admin" do
-      resources :partners, only: [:new, :create, :edit, :update, :destroy]
-      resources :annotations, only: [:new, :create, :edit, :update, :destroy]
-      resources :information_elements, only: [:new, :create, :edit, :update, :destroy]
-      resources :general_infos, only: [:create,:update]
+      resources :partners
+      resources :annotations
+      resources :information_elements
+      resources :general_infos
 
       resources :contest_nominations do 
         resources :contest_works
@@ -26,13 +25,13 @@ Rails.application.routes.draw do
   resources :information_elements, only: [:index, :show]
  
 
-  resources :contest_nominations, constraints: lambda { |req| GeneralInfo.exists?(:is_registration_active => true)} do 
-    resources :contest_works
+  resources :contest_nominations,only:[:create] ,constraints: lambda { |req| GeneralInfo.exists?(:is_registration_active => true)} do 
+    resources :contest_works,only:[:create]
   end
 
-  resources :competition_teams, constraints: lambda { |req| GeneralInfo.exists?(:is_registration_active => true)} do 
-    resources :competition_team_members
-    resources :competition_team_coach
+  resources :competition_teams,only:[:create], constraints: lambda { |req| GeneralInfo.exists?(:is_registration_active => true)} do 
+    resources :competition_team_members,only:[:create]
+    resources :competition_team_coach,only:[:create]
   end
 
 end
