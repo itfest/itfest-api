d9578b0df13316d6c2d02ef60e784bd5e6d01788
NickP <chepik29rus@gmail.com>
2018-10-26 17:52:56 +0300

    comp_team fixes

--- app/models/competition_team.rb
+++ app/models/competition_team.rb
@@ -2,7 +2,6 @@ class CompetitionTeam < ApplicationRecord
 	has_many :competition_team_members, :dependent => :destroy
 	has_one :competition_team_coach, :dependent => :destroy
 	validates :name, presence: true, uniqueness: true, length: { in: 2..16 }, custom_name: true
-	validates_length_of :competition_team_members, maximum: 3
 	accepts_nested_attributes_for :competition_team_members, limit: 3
 	accepts_nested_attributes_for :competition_team_coach, limit: 1
 end
