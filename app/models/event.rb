efe3d398ff930dfea36ff4e573a52e1c7ad8e134
NickP <chepik29rus@gmail.com>
2018-10-30 22:14:52 +0300

    small fix of event

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,4 +8,6 @@ class Event < ApplicationRecord
 	validates :has_coach, inclusion: { in: [true, false] }
 	validates :has_question, inclusion: { in: [true, false] }
 	validates :is_online_participation_available, inclusion: { in: [true, false] }
+
+	accepts_nested_attributes_for :question_list
 end
