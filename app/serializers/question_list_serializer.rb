17c2650692ca28b7502c4aa1125981ca3aefb006
NickP <chepik29rus@gmail.com>
2018-11-01 21:28:20 +0300

    add question to question_list serializer

--- app/serializers/question_list_serializer.rb
+++ app/serializers/question_list_serializer.rb
@@ -5,5 +5,5 @@ class QuestionListSerializer < ActiveModel::Serializer
 	has_many :question_answers
 	has_many :event_participation_notes, :through => :question_answers
 	
-	attributes :id
+	attributes :id, :questions
 end
