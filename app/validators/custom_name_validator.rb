0cad7d04bb84ef9fbcb1ace8469f36ef0e92c1e2
NickP <chepik29rus@gmail.com>
2018-11-02 03:54:03 +0300

    fix validator

--- app/validators/custom_name_validator.rb
+++ app/validators/custom_name_validator.rb
@@ -1,6 +1,6 @@
 class CustomNameValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /^[a-zA-Zа-яА-Я ,.'-]+$/
+    unless value =~ /^[a-zA-Zа-яА-Я ,.'-]*$/
       record.errors[attribute] << (options[:message] || "is not valid")
     end
   end
